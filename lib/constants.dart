d0caf5371fe88c42a5f653dd96a1f21bd3de72ee
sk335 <appdevsandeep@gmail.com>
2023-05-29 20:03:20 +0530

    game

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+TextStyle kCustomText({
+  double fontSize = 16.0,
+Color color = Colors.white,
+FontWeight fontWeight = FontWeight.normal
+})=> TextStyle(
+  fontSize: fontSize,
+  color: color,
+  fontWeight: fontWeight
+);
